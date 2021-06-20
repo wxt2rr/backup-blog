74f87a8faaf0fc8ac4d82b7f7f5785bed1022130
wangxt <1471520488@qq.com>
2021-06-20 13:35:12 +0800

    update info

--- shell/update.sh
+++ shell/update.sh
@@ -1,6 +1,7 @@
-#! /bin/bash
+#/bin/bash
 
-cd /usr/hexo-blog/my-hexo-blog
+cd /usr/hexo-blog/my-blog
 git pull
 docker exec hexo-blog hexo clean
 docker exec hexo-blog hexo g
+docker exec hexo-blog hexo d
